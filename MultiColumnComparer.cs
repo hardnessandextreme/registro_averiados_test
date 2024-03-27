69519483d7cdf2dacc14c522045939652acf5ec5
Jonathan Quinte <jonathandanixa@hotmail.com>
2024-03-27 10:01:07 -0500

    creacion de clase que usa una interfaz IComparer para ordenar un datagridview por sus primeras columnas.

---
+++ MultiColumnComparer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace registro_dañados
+{
+    internal class MultiColumnComparer : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            DataGridViewRow row1 = (DataGridViewRow)x;
+            DataGridViewRow row2 = (DataGridViewRow)y;
+
+            // Comparar por fecha
+            int compareResult = DateTime.Compare(Convert.ToDateTime(row1.Cells["FECHA"].Value), Convert.ToDateTime(row2.Cells["FECHA"].Value));
+            if (compareResult != 0)
+            {
+                return compareResult;
+            }
+
+            // Comparar por tipo
+            compareResult = string.Compare(row1.Cells["TIPO"].Value.ToString(), row2.Cells["TIPO"].Value.ToString());
+            if (compareResult != 0)
+            {
+                return compareResult;
+            }
+
+            // Comparar por marca
+            compareResult = string.Compare(row1.Cells["MARCA"].Value.ToString(), row2.Cells["MARCA"].Value.ToString());
+            if (compareResult != 0)
+            {
+                return compareResult;
+            }
+
+            // Comparar por modelo
+            compareResult = string.Compare(row1.Cells["MODELO"].Value.ToString(), row2.Cells["MODELO"].Value.ToString());
+            return compareResult;
+        }
+    }
+}
